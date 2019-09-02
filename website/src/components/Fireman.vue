332e9be3d7598922337d9716dac1e192524a0d89
andyruwruw <46572910+andyruwruw@users.noreply.github.com>
2019-09-02 12:59:13 -0600

    Edit - Finish

--- website/src/components/Fireman.vue
+++ website/src/components/Fireman.vue
@@ -1,58 +1,103 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="fireman">
+    <div @mouseover="noise" v-bind:class="{fireman1 : type == 1, fireman2 : type == 2, fireman3 : type == 3, fireman4 : type == 4, fireman5 : type == 5, fireman6 : type == 6, 
+    fireman7 : type == 7, fireman8 : type == 8, fireman9 : type == 9}" v-bind:style="{left: x + 'px', top: y + 'px', zIndex: speed}" id="image">
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'fireman',
   props: {
-    msg: String
+    type: Number,
+    name: Number,
+    x: Number,
+    y: Number,
+    speed: Number,
+    audio: Number,
+  },
+  methods: {
+    noise() {
+      var media = document.getElementById(this.audio);
+      media.volume = .5;
+      const playPromise = media.play();
+      if (playPromise !== null) playPromise.catch(() => {media.play();});
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+#image {
+  display: block;
+  height: 56vh;
+  width: 15vw;
+  background-size: 100% auto;
+  background-repeat: no-repeat;
+  background-position: center;
+  position: fixed;
+  transition: all .2s ease;
+  opacity: .5;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+@keyframes shake {
+  0% {
+    transform: rotate(0deg) scale(1 1);
+  }
+  25% {
+    transform: rotate(2deg) scale(1.05 1.05);
+  }
+  50% {
+    transform: rotate(0deg) scale(1.1 1.1);
+  }
+  75% {
+    transform: rotate(-2deg) scale(1.15 1.15);
+  }
+  100% {
+    transform: rotate(0deg) scale(1.2 1.2);
+  }
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+#image:hover {
+  transform: scale(1.2, 1.2);
+  animation: shake .5s ease;
+  opacity: 1;
 }
-a {
-  color: #42b983;
+
+.fireman1 {
+  background-image: url("../assets/fireman1.png");
+}
+
+.fireman2 {
+  background-image: url("../assets/fireman2.png");
+}
+
+.fireman3 {
+  background-image: url("../assets/fireman3.png");
+}
+
+.fireman4 {
+  background-image: url("../assets/fireman4.png");
+}
+
+.fireman5 {
+  background-image: url("../assets/fireman5.png");
+}
+
+.fireman6 {
+  background-image: url("../assets/fireman6.png");
+}
+
+.fireman7 {
+  background-image: url("../assets/fireman7.png");
+}
+
+.fireman8 {
+  background-image: url("../assets/fireman8.png");
+}
+
+.fireman9 {
+  background-image: url("../assets/fireman9.png");
 }
 </style>
--- website/src/views/Home.vue
+++ website/src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
-    <Fireman type="type"/>
+    <div id="background">
+      <h1>Happy Birthday Uriyah!</h1>
+      <Fireman v-for="fireman in firemen" v-bind:key="fireman.name" v-bind:type="fireman.type" v-bind:x="fireman.x" v-bind:y="fireman.y" v-bind:speed="fireman.speed"
+      v-bind:audio="fireman.audio"></Fireman>
+      <h2>Click the page and hover over firemen ;)</h2>
+    </div>
   </div>
 </template>
 
@@ -12,6 +17,91 @@ export default {
   name: 'home',
   components: {
     Fireman
-  }
+  },
+  data() {
+    return {
+      firemen: [],
+    }
+  },
+  methods: {
+    move() {
+      for (let i = 0; i < this.firemen.length; i++)
+      {
+        this.firemen[i].x += this.firemen[i].speed;
+        if (this.firemen[i].x > window.innerWidth)
+        {
+          let newFireman = {type: Math.ceil(Math.random() * 9), x: -150, y: Math.random() * window.innerHeight - 50, name: Math.round(Math.random() * 1000), speed: Math.random() * 5, audio: Math.ceil(Math.random() * 10)};
+          this.firemen.splice(i, 1, newFireman);
+        }
+      }
+    }
+  },
+  created() {
+    while(this.firemen.length < 20) {
+      let newFireman = {type: Math.ceil(Math.random() * 9), x: Math.random() * window.innerWidth, y: Math.random() * window.innerHeight - 50, name: Math.round(Math.random() * 1000), speed: Math.random() * 3, audio: Math.ceil(Math.random() * 10)};
+      this.firemen.push(newFireman);
+    }
+    setInterval(this.move, 50);
+  },
 }
 </script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Bangers&display=swap');
+
+#background {
+  display: block;
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  left: 0px;
+  top: 0px;
+  background-image: radial-gradient(rgb(0, 0, 0),rgba(0,0,0,0)), url("../assets/background.jpeg");
+  background-size: 100% auto;
+  background-repeat: repeat;
+  background-positon: center;
+}
+
+@keyframes title {
+  0% {
+    font-size: 7em;
+  }
+    50% {
+    font-size: 7.5em;
+  }
+    100% {
+    font-size: 7em;
+  }
+  
+}
+
+h1 {
+  color: white;
+  z-index: 100;
+  display: block;
+  width: 100%;
+  text-align: center;
+  position: fixed;
+  top: 10vh;
+  font-size: 7em;
+  font-family: 'Bangers', cursive;
+  background: -webkit-linear-gradient(rgb(255, 232, 128), rgb(119, 21, 21));
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  animation: title 2s linear 0s infinite;
+}
+
+
+
+h2 {
+  color: white;
+  font-size: 1.5em;
+  z-index: 100;
+  display: block;
+  width: 100%;
+  text-align: center;
+  position: fixed;
+  bottom: 20px;
+  
+}
+</style>
