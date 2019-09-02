185dd196272f5652dbc4070c0f7388bbaa360126
andyruwruw <46572910+andyruwruw@users.noreply.github.com>
2019-09-02 01:58:02 -0600

    Edit - First Commit

---
+++ website/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Fireman type="type"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Fireman from '@/components/Fireman.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Fireman
+  }
+}
+</script>
